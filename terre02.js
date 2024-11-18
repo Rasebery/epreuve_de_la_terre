df94d8bb4b8cad839ad2e13405e1d12c87d54b86
Keith Conscience <keith.conscience@gmail.com>
2024-11-18 09:43:39 +0100

    Modification of the exercice terre02.js

--- terre02.js
+++ terre02.js
@@ -1,7 +1,6 @@
-const myArgvs = process.argv.slice(2)
+const myArguments = process.argv.slice(2)
 //process.argv.slice() permet de choisir ce que l’on souhaite conserver dans le tableau.
-for (let i = 0; i < myArgvs.length; i++) {
-//myArgvs.length contient le nombre d’arguments passés dans le tableau myArgvs.
-    console.log(myArgvs[i])
-//myArgvs[i] contient les arguments du tableau myArvgs avec les expressions initialisé avec i
+for (const i of myArguments) {
+//on recupère les arguments du tableau myArguments pour les passé dans i avec l'instruction For...of
+    console.log(i)
 }
